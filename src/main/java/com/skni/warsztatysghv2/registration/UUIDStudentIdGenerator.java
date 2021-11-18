1dca22dfcc9ee704f2ecb685e8861021149c700c
jedromz <romankiewicz.j@gmail.com>
2021-11-18 21:44:20 +0100

    changed idgenerator to component

--- src/main/java/com/skni/warsztatysghv2/registration/UUIDStudentIdGenerator.java
+++ src/main/java/com/skni/warsztatysghv2/registration/UUIDStudentIdGenerator.java
@@ -1,10 +1,11 @@
 package com.skni.warsztatysghv2.registration;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
-@Service
+@Component
 public class UUIDStudentIdGenerator {
     public String getNext() {
         return UUID.randomUUID().toString();
